a9ca5b5437c937cc034370aef3eca42a55cd6e27
Giovani Generali <giovani.generali@gmail.com>
2019-03-16 00:01:59 -0300

    add saga

--- modulo3/src/store/index.js
+++ modulo3/src/store/index.js
@@ -1,17 +1,35 @@
-import { createStore, compose } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 
 import reducers from "./reducers";
+import sagas from "./sagas";
+
+const middlewares = [];
+
+const sagaMiddleware = createSagaMiddleware();
+
+middlewares.push(sagaMiddleware);
 
 const tronMiddleware =
   process.env.NODE_ENV === "development"
     ? console.tron.createEnhancer
     : () => {};
 
-const composeEnhancers =
+const reduxDevToolsExtension =
   process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : compose;
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    : null;
 
-const store = createStore(reducers, composeEnhancers(tronMiddleware()));
+const store = createStore(
+  reducers,
+  compose(
+    tronMiddleware(),
+    applyMiddleware(...middlewares),
+    reduxDevToolsExtension
+  )
+);
+
+sagaMiddleware.run(sagas);
 
 export default store;
